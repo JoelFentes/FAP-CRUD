146d8bfbba0fcb453384f5e1416bcdf969b6cc2c
Joel Fentes <82660102+JoelFentes@users.noreply.github.com>
2023-09-18 12:35:28 +0000

    #

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ let matricula = 0
 let professorID = 0
 
 
-
 class CadastroAluno {
     constructor(matricula, nomeAluno, cpfAluno, numTelefoneAluno, planoAcademia, enderecoAluno){
         this.matricula = matricula
